617d227fd185ad890057d6e61d6cd06cf33fa0c0
nayan-pradhan <nayan.pradhan@hotmail.com>
2022-11-24 11:41:01 +0100

    Adding free solo program

---
+++ src/free_solo_program.py
@@ -0,0 +1,16 @@
+'''
+    Master Free Solo Program
+'''
+
+from control.free_solo_ctrl import *
+
+class FreeSoloProgram():
+
+    def __init__(self,
+
+    ):
+        print("Running Free Solo Control. You may move platform around.")
+        FreeSoloClass()
+
+if __name__ == '__main__':
+    FreeSoloProgram()
